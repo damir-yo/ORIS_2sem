16a246281eaacab7be6efb2a8bbab3f427971a01
Gilmanov Damir <z-damka@MacBook-Air-Gilmanov.local>
2026-02-26 22:17:43 +0300

    dz2

---
+++ src/main/java/com/itis/dz2/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.itis.dz2.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.itis")
+public class AppConfig {
+}
