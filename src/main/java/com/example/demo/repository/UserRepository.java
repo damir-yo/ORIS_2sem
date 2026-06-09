621b7f6f2ebc6fc981962a46781def500fc58c87
Gilmanov Damir <z-damka@MacBook-Air-Gilmanov.local>
2026-06-10 02:07:35 +0300

    почистил для красоты, тк все дз лежат на отдельных ветках

--- src/main/java/com/example/demo/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface UserRepository extends JpaRepository<UserEntity, Long> {
-}
